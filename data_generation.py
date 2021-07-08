9f6d62b5fae0c16ac794bcf13f661e412d042905
lbriand <lbriand@deezer.com>
2021-07-08 16:07:45 +0200

    change names

--- data_generation.py
+++ data_generation.py
@@ -18,7 +18,7 @@ def generate(master_path):
 
     #songs
 
-    song_embeddings_path = dataset_path + "/song_embeddings_sample.parquet"
+    song_embeddings_path = dataset_path + "/song_embeddings.parquet"
     song_embeddings = pd.read_parquet(song_embeddings_path, engine = 'fastparquet').fillna(0)
 
     if not os.path.exists(master_path+"/m_song_dict.pkl"):
@@ -32,17 +32,17 @@ def generate(master_path):
 
     # user embeddings (target = only for train users)
 
-    user_embeddings = pd.read_parquet(dataset_path + "/user_embeddings_sample.parquet", engine = 'fastparquet')
+    user_embeddings = pd.read_parquet(dataset_path + "/user_embeddings.parquet", engine = 'fastparquet')
     list_embeddings = ["embedding_"+str(i) for i in range(len(user_embeddings["svd_embeddings"][0]))]
     user_embeddings[list_embeddings] = pd.DataFrame(user_embeddings.svd_embeddings.tolist(), index= user_embeddings.index)
     embeddings_train = user_embeddings[list_embeddings].values
 
     # user features train
 
-    features_train_path = dataset_path + "/user_features_train_sample.parquet"
+    features_train_path = dataset_path + "/user_features_train.parquet"
     features_train = pd.read_parquet(features_train_path, engine = 'fastparquet').fillna(0)
     features_train = features_train.sort_values("user_index")
-    features_train_ = features_train.values[:,2:]
+    features_train = features_train.reset_index(drop=True)#to check it is ok for train data
 
     # training dataset creation
 
@@ -57,72 +57,27 @@ def generate(master_path):
         pickle.dump(x_train, open("{}/{}/x_train_{}.pkl".format(master_path, state, idx), "wb"))
         pickle.dump(y_train, open("{}/{}/y_train_{}.pkl".format(master_path, state, idx), "wb"))
 
-    # user features validation
+    # user features validation & test
 
-    features_validation_path = dataset_path + "/user_features_validation_sample.parquet"
-    features_validation = pd.read_parquet(features_validation_path, engine = 'fastparquet').fillna(0)
-    features_validation = features_validation.sort_values("user_index")
-    features_validation = features_validation.reset_index(drop=True)
+    states = ["validation", "test"]
+    for state in states :
+        features_validation_path = dataset_path + "/user_features_" + state + ".parquet"
+        features_validation = pd.read_parquet(features_validation_path, engine = 'fastparquet').fillna(0)
+        features_validation = features_validation.sort_values("user_index")
+        features_validation = features_validation.reset_index(drop=True)
 
-    state = "validation"
-    if not os.path.exists(master_path+"/"+state+"/"):
-        os.mkdir(master_path+"/"+state+"/"+"/")
-    for i in range(len(features_validation)):
-        x_validation = torch.FloatTensor(features_validation.iloc[i,2:])
-        y_validation = [song_dict[song_index]  for song_index in features_validation["d1d30_songs"][i]]
-        groundtruth_validation_list = [1.0*
-                                       (song in y_validation)
-                                       for song in range(len(song_embeddings))]
-        pickle.dump(x_validation, open("{}/{}/x_validation_{}.pkl".format(master_path, state, i), "wb"))
-        pickle.dump(y_validation, open("{}/{}/y_listened_songs_validation_{}.pkl".format(master_path, state, i), "wb"))
-        pickle.dump(groundtruth_validation_list, open("{}/{}/groundtruth_list_{}.pkl".format(master_path, state, i), "wb"))
+        if not os.path.exists(master_path+"/"+state+"/"):
+            os.mkdir(master_path+"/"+state+"/"+"/")
+        for i in range(len(features_validation)):
+            x_validation = torch.FloatTensor(features_validation.iloc[i,2:])
+            y_validation = [song_dict[song_index]  for song_index in features_validation["d1d30_songs"][i]]
+            groundtruth_validation_list = [1.0 * (song in y_validation) for song in range(len(song_embeddings))]
+            pickle.dump(x_validation, open("{}/{}/x_validation_{}.pkl".format(master_path, state, i), "wb"))
+            pickle.dump(y_validation, open("{}/{}/y_listened_songs_validation_{}.pkl".format(master_path, state, i), "wb"))
+            pickle.dump(groundtruth_validation_list, open("{}/{}/groundtruth_list_{}.pkl".format(master_path, state, i), "wb"))
 
 
 ''''
-
-
-# validation data
-
-features_validation_path = data_path + "/tableValidationMF/part-00000-50e8c107-cf8f-4813-919b-70a5b364b827-c000.snappy.parquet"
-features_validation = pd.read_parquet(features_validation_path, engine = 'fastparquet').fillna(0)
-features_validation = features_validation.sort_values("user_index")
-features_validation = features_validation.reset_index(drop=True)
-
-idx = 70000
-user_list = dict()
-state = "validation"
-if not os.path.exists(master_path+"/"+state+"/"):
-    os.mkdir(master_path+"/"+state+"/"+"/")
-for i in range(len(features_validation)):
-    if idx % 1000 == 0:
-        print(idx)
-    x_validation = torch.FloatTensor(features_validation.iloc[i,2:])
-    y_validation = [song_dict[song_index]  for song_index in features_validation["d1d30_songs"][i]]
-
-    pickle.dump(x_validation, open("{}/{}/x_validation_{}.pkl".format(master_path, state, idx), "wb"))
-    pickle.dump(y_validation, open("{}/{}/y_listened_songs_validation_{}.pkl".format(master_path, state, idx), "wb"))
-
-    idx += 1
-
-# groundtruth of validation data : songs listened after d0 between D1 and D30
-
-idx = 70000
-user_list = dict()
-state = "validation"
-if not os.path.exists(master_path+"/"+state+"/"):
-    os.mkdir(master_path+"/"+state+"/"+"/")
-for i in range(len(features_validation)):
-    if idx % 1000 == 0:
-        print(idx)
-    y_validation = [song_dict[song_id]
-                    for song_id in features_validation["d1d30_songs"][i]]
-    groundtruth_validation_list = [1.0*
-                                   (song in y_validation)
-                                   for song in range(len(song_embeddings))]
-    pickle.dump(groundtruth_validation_list, open("{}/{}/groundtruth_list_{}.pkl".format(master_path, state, idx), "wb"))
-
-    idx += 1
-
 
 # test data
 
