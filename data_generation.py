4cc6f80139b3dbd622d158fc4477a8f0a6e7a7e2
lbriand <lbriand@deezer.com>
2021-07-08 11:50:08 +0200

    code data train

--- data_generation.py
+++ data_generation.py
@@ -15,7 +15,10 @@ from sklearn.preprocessing import Normalizer
 
 def generate(master_path):
     dataset_path = os.getcwd() + "/data"
-    song_embeddings_path = dataset_path + "/song_embeddings.parquet"
+
+    #songs
+
+    song_embeddings_path = dataset_path + "/song_embeddings_sample.parquet"
     song_embeddings = pd.read_parquet(song_embeddings_path, engine = 'fastparquet').fillna(0)#, columns=dataset_columns)
 
     if not os.path.exists(master_path+"/m_song_dict.pkl"):
@@ -25,6 +28,33 @@ def generate(master_path):
         pickle.dump(song_dict, open("{}/m_song_dict.pkl".format(master_path), "wb"))
     else:
         song_dict = pickle.load(open("{}/m_song_dict.pkl".format(master_path), "rb"))
+
+    # user embeddings (target = only for train users)
+
+    user_embeddings = pd.read_parquet(dataset_path + "user_embeddings_sample.parquet", engine = 'fastparquet')
+    list_embeddings = ["embedding_"+str(i) for i in range(len(user_embeddings["mf_embeddings"][0]))]
+    user_embeddings[list_embeddings] = pd.DataFrame(user_embeddings.mf_embeddings.tolist(), index= user_embeddings.index)
+    embeddings_train = user_embeddings[list_embeddings].values
+
+    # user features train
+
+    features_train_path = dataset_path + "/user_features_train_sample.parquet"
+    features_train = pd.read_parquet(features_train_path, engine = 'fastparquet').fillna(0)
+    features_train = features_train.sort_values("user_index")
+    features_train_ = features_train.values[:,2:]
+
+    # training dataset creation
+
+    state = "train"
+    if not os.path.exists(master_path+"/"):
+        os.mkdir(master_path+"/")
+    if not os.path.exists(master_path+"/"+state+"/"):
+        os.mkdir(master_path+"/"+state+"/")
+    for idx in range(len(features_train)):
+        x_train = torch.FloatTensor(features_train.iloc[idx,2:])
+        y_train = torch.FloatTensor(user_features[list_embeddings].iloc[idx,:])
+        pickle.dump(x_train, open("{}/{}/x_train_{}.pkl".format(master_path, state, idx), "wb"))
+        pickle.dump(y_train, open("{}/{}/y_train_{}.pkl".format(master_path, state, idx), "wb"))
 
 
 ''''
@@ -42,10 +72,12 @@ list_embeddings = ["embedding_"+str(i) for i in range(len(user_features["mf_feat
 user_features[list_embeddings] = pd.DataFrame(user_features.mf_features.tolist(), index= user_features.index)
 embeddings_train = user_features[list_embeddings].values
 
+-
 features_train_path = data_path + "/tableTrainMF/part-00000-f9d99b97-621a-434b-8c9d-92be08c37e87-c000.snappy.parquet"
 features_train = pd.read_parquet(features_train_path, engine = 'fastparquet').fillna(0)
 features_train = features_train.sort_values("user_index")
 features_train_ = features_train.values[:,2:]
+-
 
 idx = 0
 user_list = dict()
